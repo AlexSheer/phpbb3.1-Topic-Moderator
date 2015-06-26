f4124af5c6b8058a3ade6e015076c709aa8b352c
AlexSheer <alex_sheer@inbox.lv>
2015-06-26 22:30:15 +0300

    Update topicmod_lng.php

--- language/ru/topicmod_lng.php
+++ language/ru/topicmod_lng.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package phpBB Extension - My Test
+* @package phpBB Extension - Topic Moderator
 * @copyright (c) 2013 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
